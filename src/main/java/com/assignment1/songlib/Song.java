1a5a9a973b910788477411b39f3cb6ea63af86a2
alex101266 <123588114+alex101266@users.noreply.github.com>
2023-02-14 14:56:48 -0500

    Adding song object file

---
+++ src/main/java/com/assignment1/songlib/Song.java
@@ -0,0 +1,40 @@
+public class Song {                 //song object
+    String name,artist,album,year;  //song, artist, and album names as well as year
+    
+    //year is a String because it becomes easier to work with; inputs will be strings so no need to convert
+    //plus, if there is no year it is easier to type "..." instead of some random int to denote that there is
+    //no year
+
+    //set it up so if they leave album or year boxes blank it somehow returns null
+    public Song(String name, String artist, String album, String year){
+        this.name = name;
+        this.artist = artist;
+        //next two check if there was an album or year input, which are optional
+        if(album == null){
+            this.album = " ";   }
+        else{
+            this.album=album;   }
+        if(year == null){
+            this.year = " ";    }
+        else{
+                this.year=year; }   }
+
+    //getter methods
+    public String getName(){
+        return this.name;}
+    public String getArtist(){
+        return this.artist;}
+    public String getAlbum(){
+        return this.album;}
+    public String getYear(){
+        return this.year;}
+
+    //setter methods (for editing)
+    public void setName(String name){
+        this.name = name;   }
+    public void setArtist(String artist){
+        this.artist = artist;   }
+    public void setAlbum(String album){
+        this.album = album;   }
+    public void setYear(String year){
+        this.year = year;   }   }
