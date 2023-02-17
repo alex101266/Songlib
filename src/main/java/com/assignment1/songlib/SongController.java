bb2fc7cbf284572ec84f5baa27bfa630bf2024bd
AardvarkSmeller <akhtemov.timur@yahoo.com>
2023-02-16 21:27:23 -0500

    Added controller fields and methods

--- src/main/java/com/assignment1/songlib/SongController.java
+++ src/main/java/com/assignment1/songlib/SongController.java
@@ -1,14 +1,58 @@
 package com.assignment1.songlib;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class SongController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button AddBtn;
+
+    @FXML
+    private TextField AlbumField;
+
+    @FXML
+    private TextField ArtistField;
+
+    @FXML
+    private Button DeleteBtn;
+
+    @FXML
+    private Button EditBtn;
+
+    @FXML
+    private Label ErrorMsg;
+
+    @FXML
+    private TextArea SongDetails;
+
+    @FXML
+    private ListView<?> SongList;
+
+    @FXML
+    private TextField SongNameField;
+
+    @FXML
+    private TextField YearField;
+
+    @FXML
+    void addSong(ActionEvent event) {
+
     }
+
+    @FXML
+    void deleteSong(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editSongDetails(ActionEvent event) {
+
+    }
+
 }
